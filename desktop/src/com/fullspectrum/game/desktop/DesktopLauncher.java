39e63765587403d154512f3a92e5482dc136c301
TJ Calabrese <TJ Calabrese>
2016-08-08 23:06:51 -0400

    Test Commit

--- desktop/src/com/fullspectrum/game/desktop/DesktopLauncher.java
+++ desktop/src/com/fullspectrum/game/desktop/DesktopLauncher.java
@@ -8,15 +8,15 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
-		config.resizable = false;
-		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
-		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+//		config.resizable = false;
+//		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+//		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 		config.vSyncEnabled = false;
 		config.foregroundFPS = 0;
 		config.backgroundFPS = 0;
-//		config.resizable = true;
-//		config.width = 1600;
-//		config.height = 900;
+		config.resizable = true;
+		config.width = 1600;
+		config.height = 900;
 		new LwjglApplication(new GdxGame(), config);
 	}
 }
